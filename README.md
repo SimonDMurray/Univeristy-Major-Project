33b98e41588bc922f5479fc24dd0892eba9bc9e8
Simon Murray <simondonaldmurray@gmail.com>
2022-10-03 12:52:41 +0000

    added getting data script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Univeristy-Major-Project
 Evidence of all scripts used in University Major Project for evidencing in University report
+
+Script Explanations:
+
+get-data.sh - this script downloads the TAR file containing cellranger outputs, removes data not needed for this project and reorganises the data into a readable manner that Seurat expects.
---
+++ get-data.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Downloading CITEseq dataset
+
+mkdir -p data
+wget -O data/GSE164378_RAW.tar 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE164378&format=file'
+tar -xvf data/GSE164378_RAW.tar -C data/
+
+# Removing HTO data as not needed for this project
+rm -rf data/*HTO*
+
+# Removing TCR data as not needed for this project
+rm -rf data/*TCR*
+
+# Removing BCR data as not needed for this project
+rm -rf data/*BCR*
+
+# Removing 3' data as discussed in project meeting 3 
+rm -rf data/*5P*
+
+# Organising data
+mkdir -p data/RNA
+mv data/*RNA*.gz data/RNA
+
+mkdir -p data/ADT
+mv data/*ADT*.gz data/ADT/
+
+# Changing files name to expected input for Seurat
+cd data/RNA
+for file in *; do
+        mv $file ${file##GSM5008737_RNA_3P-}
+done
+
+cd ../ADT
+for file in *; do
+	mv $file ${file#GSM5008738_ADT_3P-}
+done
+
+# Removing TAR file
+rm data/GSE164378_RAW.tar
